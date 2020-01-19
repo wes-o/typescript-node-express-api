1abc6f846331dac1ef230ebd8af1feff2823c502
wes-o <weso.turn@gmail.com>
2020-01-18 20:44:45 -0600

    added initial typings folder

---
+++ typings/index.d.ts
@@ -0,0 +1,17 @@
+/*
+ * Start a new declaration file @type here
+*/
+
+export var Bar: { a: Bar };
+
+export interface Bar {
+    count: number;
+} 
+
+export interface Foo {
+    x: number;
+}
+
+export interface Foo {
+    y: number;
+}
